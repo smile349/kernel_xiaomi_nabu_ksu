a06115e9b6df676450ee70c6c055e30725811c53
undefined <undefined_1@outlook.com>
2024-02-22 18:49:12 +0800

    feat: Change anykernel.sh to make it don't touch my dtbo and verndor_boot

--- CosmicFresh/anykernel.sh
+++ CosmicFresh/anykernel.sh
@@ -46,20 +46,3 @@ dump_boot;
 
 write_boot;
 ## end boot install
-
-
-# shell variables
-block=vendor_boot;
-is_slot_device=1;
-ramdisk_compression=auto;
-patch_vbmeta_flag=auto;
-
-# reset for vendor_boot patching
-reset_ak;
-
-
-## AnyKernel vendor_boot install
-split_boot; # skip unpack/repack ramdisk since we don't need vendor_ramdisk access
-
-flash_boot;
-## end vendor_boot install
