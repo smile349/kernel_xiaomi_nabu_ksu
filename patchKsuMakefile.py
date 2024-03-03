8df66f54d882755ec90388194b88998ff5b41d60
undefined <undefined_1@outlook.com>
2024-03-03 18:20:57 +0800

    add ksu makefile patch

---
+++ patchKsuMakefile.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+ORIGIN = """$(shell cd $(srctree)/$(src); /usr/bin/env PATH=\"$$PATH\":/usr/bin:/usr/local/bin [ -f ../.git/shallow ] && git fetch --unshallow)
+KSU_GIT_VERSION := $(shell cd $(srctree)/$(src); /usr/bin/env PATH="$$PATH":/usr/bin:/usr/local/bin git rev-list --count HEAD)"""
+
+with open("KernelSU/kernel/Makefile", "r+") as f:
+    content = f.read()
+    content = content.replace(
+        ORIGIN, 
+        "KSU_GIT_VERSION := $(shell cd $(srctree)/$(src); /usr/bin/env PATH=\"$$PATH\"" \
+        ":/usr/bin:/usr/local/bin [ -f ../.git/shallow ] && git fetch --unshallow; git rev-list --count HEAD)"
+    )
+    f.seek(0)
+    f.write(content)
+    f.close()
